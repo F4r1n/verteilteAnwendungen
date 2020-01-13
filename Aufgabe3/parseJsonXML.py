a2d7ab8ee63ff95d7111b0c66e939641d11994db
Jonas Liebermann <jonas.liebermann@senseaition.com>
2020-01-13 14:24:55 +0100

    victorscripts

--- Aufgabe3/parseJsonXML.py
+++ Aufgabe3/parseJsonXML.py
@@ -2,12 +2,13 @@ import xml.dom.minidom
 import json
 
 # work around since I had some issues with .json files
-with open("json.txt", "r", encoding="utf-8") as jsonFile:
+#edit: turns out it works with json files too
+with open("./Aufgabe3/s0563822_Pansegrau.json", "r", encoding="utf-8") as jsonFile:
     data = json.load(jsonFile)
     for key in data.items():
         print(key)
 
 
-dom = xml.dom.minidom.parse("s05635822_Pansegrau.xml")
+dom = xml.dom.minidom.parse("./Aufgabe3/s0563822_Pansegrau.xml")
 pretty_xml_as_string = dom.toprettyxml()
 print(pretty_xml_as_string)
